e3cb74b6adbc96cc0e32413786ac230ee44bb220
ArmaganArslan <93640981+ArmaganArslan@users.noreply.github.com>
2024-11-07 00:25:50 +0300

    Initial commit

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Camera : MonoBehaviour
+{
+    public Transform ballPosition;
+    Vector3 diff;
+
+    void Start()
+    {
+        diff = transform.position - ballPosition.position;
+    }
+    void LateUpdate()
+    {
+        if (!Ball.isFall)
+        {
+            transform.position = ballPosition.position + diff;
+        }
+    }
+}
---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject last_cube;
+    
+    void Start()
+    {
+        for (int i = 0; i < 25; i++)
+        {
+            create_cube();
+        }
+    }
+
+    public void create_cube()
+    {
+        Vector3 direction;
+
+        if (Random.Range(0, 2) == 0)
+        {
+            //direction = Vector3.left;
+            transform.Rotate(new Vector3(0, 45, 45));
+        }
+
+        else
+        {
+            direction = Vector3.forward;
+        }
+
+        //last_cube = Instantiate(last_cube, last_cube.transform.position + direction, last_cube.transform.rotation);
+    }
+}
